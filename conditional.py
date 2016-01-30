c7dc4418bff899f22b70582fe6cea202953bb981
AndyYuanYe <yuanyeandy@gmail.com>
2016-01-30 16:30:49 -0700

    initial commit

---
+++ conditional.py
@@ -0,0 +1,9 @@
+x=7
+day = "Monday"
+
+if day or x:
+    print("The day is" + day + ". What a great day!")
+    print("That also makes sense")
+else:
+    print("That will not happen")
+
---
+++ iteration.py
@@ -0,0 +1,8 @@
+books = ["Tale of Two Cities", "Harry Potter", "Lord of the Ring"]
+
+books[0] = "property"
+
+for b in books:
+    print(b);
+    print(b);
+    print(b)
